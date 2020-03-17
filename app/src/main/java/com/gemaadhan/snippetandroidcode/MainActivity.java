cdaf6086e227464d2ca17c8e556f478a30494ff6
Gema Adhan <gemaadhan@gmail.com>
2020-03-18 05:57:49 +0700

    ok

--- app/src/main/java/com/gemaadhan/snippetandroidcode/MainActivity.java
+++ app/src/main/java/com/gemaadhan/snippetandroidcode/MainActivity.java
@@ -3,6 +3,10 @@ package com.gemaadhan.snippetandroidcode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> city =  new ArrayList<>();
+        city.add("Tokyo");
+        city.add("Beijing");
+        city.add("San Francisco");
+        city.add("Milan");
+        city.add("Paris");
+        city.add("London");
+        city.add("Liverpool");
+        city.add("Manchester");
+        city.add("New Dheli");
+        city.add("Manila");
+
+        ListView cityListView = (ListView) findViewById(R.id.list);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, city);
+        cityListView.setAdapter(adapter);
     }
 }
